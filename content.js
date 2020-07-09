a129188aff91d64c33e24c55144229ab3f399559
HarmonicChange <peiyi.lin2473@gmail.com>
2020-07-08 23:10:50 -0700

    Add core files

---
+++ content.js
@@ -0,0 +1,6 @@
+// console.log("Content script running");
+
+var documentAsHTML = document.getElementsByTagName('html').innerHTML;
+console.log(documentAsHTML); // returns an HTMLCollection
+
+// TODO Get HTML tags with no children.
