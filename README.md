ae024f557d3d84112d7b285a04695cf8b7d6b86c
HarmonicChange <peiyi.lin2473@gmail.com>
2020-07-09 17:20:43 -0700

    Add function for constructing regex, list assumptions in README

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 A Chrome Extension that will automatically detect ingredients on websites and convert them to the desired units.
 
 Ex: "1 cup flour" --> "1 cup (120 grams) flour"
+
+## Matching Algorithm
+
+This script uses regular expression to match ingredients on websites. Currently, the following assumptions are made.
+* An ingredient is always formatted as "[fraction or digit] [measurement word] [ingredient word]"
+* Fractions only have up to 2 digits in the numerator and denominator. Non-fractional numbers only have up to 2 digits.
+* The entire phrase is contained within the innerHTML of a childless HTML tag.
+
+Some possible expansions to our matching regular expression:
+* Other formats for writing ingredients, for example, "Flour, 1 cup".
--- content.js
+++ content.js
@@ -5,13 +5,41 @@ var htmlTagsAsArray = Array.prototype.slice.call(allHTMLTags); // Converts to ar
 var filteredHTML = htmlTagsAsArray.filter(node => node.childElementCount == 0 && node.innerHTML && node.tagName != "SCRIPT");
 console.log(filteredHTML);
 
-// Contains words that we will "know" how to convert. Includes measurements and ingredients
-var vocabulary = ["cup", "cups",
-    "ounce", "ounces", "oz",
-    "teaspoon", "teaspoons", "tspn",
-    "flour", "bread flour", "cake flour", "all purpose flour"];
-
-// Loop through filtered elements and find any occurrences of vocabulary
+// Loop through inneHTML and find phrases that we will convert
 //for (var i = 0; i < filteredHTML.length; i++) {
 
 //}
+
+// Uses vocabulary we define to build a regular expression for detecting our desired phrases.
+function assembleRegexFromVocab () {
+  // Make sure to start with the LONGEST version of the word first!
+  var measurements_vocab = ["cups", "cup",
+      "ounces", "ounce", "oz",
+      "teaspoons", "teaspoon", "tspn",
+      "tablespoons", "tablespoon", "tbspn", "tbsp"];
+  var ingredients_vocab = ["bread flour", "cake flour", "all purpose flour", "flour",
+      "brown sugar", "granulated sugar", "cane sugar", "sugar"];
+
+  // Assemble a regex string that will match "[fraction/digit] [measurement vocab] [ingredient vocab]"
+  var myRegexString = "/(\d{1,2}\/\d{1,2}|\d{1,2}) ("; // Start with a fraction or digit
+  for (var i = 0; i < measurements_vocab.length; i++) {
+    if (i == 0) { // On first iteration, we don't add the | symbol for the regex
+      myRegexString += measurements_vocab[i];
+    } else {
+      myRegexString += "|" + measurements_vocab[i];
+    }
+  }
+  myRegexString += ") (";
+  for (var i = 0; i < ingredients_vocab.length; i++) {
+    if (i == 0) { // On first iteration, we don't add the | symbol for the regex
+      myRegexString += ingredients_vocab[i];
+    } else { // Otherwise, we or the rest of the words
+      myRegexString += "|\b" + ingredients_vocab[i];
+    }
+  }
+  myRegexString += ")/i";
+
+  // Return the string as a regular expression
+  console.log(myRegexString);
+  return new RegExp(myRegexString);
+}
